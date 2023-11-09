0c9df4b69d74d6fc8f395a73305c8c9cd6605595
Reza Ghandchi <rghandchi@yahoo.com>
2023-11-09 08:24:14 -0500

    commit validation script

---
+++ Validate Data Load.sql
@@ -0,0 +1,7 @@
+select * from [DimCustomer]
+select * from [DimTeam] 
+select * from [DimAgent]
+select * from [DimCallType]
+select * from [DimDate]
+select * from [DimTime]
+select * from [FactCall]
